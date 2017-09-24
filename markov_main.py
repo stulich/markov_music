3bcdf7ba2ca5dfda258054c6977436a20f859811
stulich <stephenulich@gmail.com>
2017-09-24 17:28:06 -0400

    Change to work with music data

--- markov_main.py
+++ markov_main.py
@@ -6,8 +6,8 @@ def randomizer(bound):
     return random.randint(1, bound)
 
 if __name__ == "__main__":
-    file_name = 'books/aladdin.txt'
+    file_name = 'music_notes.txt'
     chain = builder.build(file_name)
     num_words = 40
     outstr = generator.generate(chain, randomizer, num_words, builder.NONWORD)
-    print(outstr)
+    print(outstr[2::])
